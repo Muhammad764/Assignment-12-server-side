63f2510e671bff5030e44b77219251ddd7754802
Muhammad764 <farhadislam745@gmail.com>
2022-05-25 15:45:02 +0600

    service detail

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -21,6 +21,13 @@ async function run() {
             const services = await cursor.toArray()
             res.send(services)
         })
+      
+      app.get('/service/:id', async (req, res) => {
+         const id = req.params.id
+         const query = {_id: ObjectId(id)}
+         const service = await servicesCollection.findOne(query)
+         res.send(service)
+       })
     }
     finally {
         
@@ -34,5 +41,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Leather websitelistening on port ${port}`)
+  console.log(`Leather website listening on port ${port}`)
 })
